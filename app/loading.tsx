62f9b4dd791545676de49006544089e33c421c42
Bluefire <public@jellezwie.rs>
2025-11-09 01:33:52 +0100

    Hide footer on loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,5 @@
 
+import FooterController from "@/components/footer/footer-controller"
 import { Spinner } from "@/components/ui/spinner"
 import Image from "next/image"
 
@@ -7,6 +8,7 @@ export default function Loading() {
         <div className="w-full h-svh flex flex-col justify-center items-center gap-8">
             <Image src="/logo.svg" alt="Logo" width={128} height={128}/>
             <Spinner/>
+            <FooterController visible={false}/>
         </div>
     )
   }
